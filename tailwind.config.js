867a8e85b2f28ef3bb489f26a9c10a4dab8d1874
anugrahp13 <anugrahprastyo13@gmail.com>
2024-04-06 11:08:37 +0700

    build - create folder and file

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/components/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+  ],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        'primary': '#14b8a6',
+        'secondary': '#64748b',
+        'dark': '#0f172a',
+      },
+      screens: {
+        '2xl' : '1320px'
+      },
+      fontFamily: {
+        poppins : ['Poppins', 'sans-serif']
+      },
+    },
+  },
+  plugins: [],
+}
+
